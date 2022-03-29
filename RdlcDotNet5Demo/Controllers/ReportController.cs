68c6ce007291cfdf80a0660f9e856568c9204584
DalimAli <dalim3281@gmail.com>
2022-03-30 00:28:05 +0600

    Add project files.

---
+++ RdlcDotNet5Demo/Controllers/ReportController.cs
@@ -0,0 +1,61 @@
+﻿using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RdlcDotNet5Demo.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ReportController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        }
+
+        // GET: HomeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        private readonly IEnumerable<Employee> employees = Employee.Gets();
+
+
+
+        // GET: HomeController/Details/5
+        public ActionResult SimpleReport()
+        {
+            var path = $"{_webHostEnvironment.ContentRootPath}" + "\\Reports" + "\\SimpleReport.rdlc";
+            LocalReport localReport = new LocalReport(path);
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            param.Add("ReportTitle", "a simple report to start with dot net core rdlc");
+
+            localReport.AddDataSource("DataSet", employees);
+
+            var result = localReport.Execute(RenderType.Pdf, 1, param, "");
+            return File(result.MainStream, "application/pdf");
+        }
+
+        public ActionResult GroupReport()
+        {
+            var path = $"{_webHostEnvironment.ContentRootPath}" + "\\Reports" + "\\GroupReport.rdlc";
+            LocalReport localReport = new LocalReport(path);
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            param.Add("ReportTitle", "Group By Rdlc Report");
+
+            localReport.AddDataSource("GroupDataSet", employees);
+
+            var result = localReport.Execute(RenderType.Pdf, 1, param, "");
+            return File(result.MainStream, "application/pdf");
+        }
+
+
+    }
+}
---
+++ RdlcDotNet5Demo/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RdlcDotNet5Demo
+{
+    public class Employee
+    {
+        public string EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Designation { get; set; }
+        public string Department { get; set; }
+
+        public static IEnumerable<Employee> Gets()
+        {
+            return new List<Employee>()
+            {
+                new Employee()
+                {
+                    EmployeeId = "1001",
+                    EmployeeName = "Md. Dalim Ali",
+                    Designation = "Software Engineer",
+                    Department = "Alico Claim"
+                },new Employee()
+                {
+                    EmployeeId = "1002",
+                    EmployeeName = "Md. Sumon Ali",
+                    Designation = "Engineer",
+                    Department = "Enterprise"
+                },new Employee()
+                {
+                    EmployeeId = "1003",
+                    EmployeeName = "Md. Saddam Hossain",
+                    Designation = "Software Engineer",
+                    Department = "Alico Claim"
+                },
+                new Employee()
+                {
+                    EmployeeId = "1004",
+                    EmployeeName = "Md. Jafor Ali",
+                    Designation = "Engineer",
+                    Department = "Enterprise"
+                },
+                new Employee()
+                {
+                    EmployeeId = "1005",
+                    EmployeeName = "Md. Khaled",
+                    Designation = "Software Engineer",
+                    Department = "Enterprise"
+                },
+                new Employee()
+                {
+                    EmployeeId = "1006",
+                    EmployeeName = "Md. Hossain Ali",
+                    Designation = "Tech Software Engineer",
+                    Department = "Enterprise"
+                },
+            };
+        }
+    }
+}
